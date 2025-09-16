a24764b29ca64387b756a2b1984497d53ef43a40
sergejlembke <sergej.lembke@posteo.de>
2025-09-16 23:07:07 +0200

    Add weather ETL DAG to orchestrate data extraction, transformation, and loading

---
+++ dags/weather-etl-dag.py
@@ -0,0 +1,31 @@
+from datetime import datetime
+
+from airflow import DAG
+from airflow.providers.standard.operators.python import PythonOperator
+
+from scripts.fetch_weather import get_weather
+from scripts.load_weather import load_weather_to_postgres
+from scripts.transform_weather import transform_weather
+
+with DAG(
+        dag_id='weather-etl-dag',
+        start_date=datetime(2025, 9, 1),
+        schedule="0,5,10,15,20,25,30,35,40,45,50,55 * * * *",
+        catchup=False
+):
+    extract_weather_from_api = PythonOperator(
+        task_id='extract_weather_from_api_task',
+        python_callable=get_weather,
+    )
+
+    transform_weather_data = PythonOperator(
+        task_id='transform_weather_data_task',
+        python_callable=transform_weather
+    )
+
+    load_weather_to_postgres = PythonOperator(
+        task_id='load_weather_to_postgres_task',
+        python_callable=load_weather_to_postgres
+    )
+
+    extract_weather_from_api >> transform_weather_data >> load_weather_to_postgres
