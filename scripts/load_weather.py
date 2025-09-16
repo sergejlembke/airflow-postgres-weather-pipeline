8d3157194755a32b42250b21814539ceb7494622
sergejlembke <sergej.lembke@posteo.de>
2025-09-16 23:06:32 +0200

    Refactor load_weather_to_postgres function to improve logging and update data insertion logic

--- scripts/load_weather.py
+++ scripts/load_weather.py
@@ -1,9 +1,11 @@
+import json
+
 import psycopg2
 
 
-def load_weather_to_postgres(ti, city="Berlin"):
-    ti.log.info("Loading weather data")
-    data = ti.xcom_pull(task_ids='transform_weather')
+def load_weather_to_postgres(ti):
+    print("Start: Loading weather data")
+    data = ti.xcom_pull(task_ids='transform_weather_data_task', key='transformed')
 
     # If running this script on the host machine, use localhost and port 5432.
     # If running inside the Airflow container, use host="postgres" (the docker-compose service name).
@@ -11,31 +13,27 @@ def load_weather_to_postgres(ti, city="Berlin"):
         dbname="weather",
         user="airflow",
         password="airflow",
-        host="localhost",
-        port=5432,
+        host="postgres"
     )
     cur = conn.cursor()
 
     cur.execute(
         """
         INSERT INTO weather_raw (city, datetime, temperature, humidity, wind_speed, raw_json)
-        VALUES (%s,
-                %s,
-                %s,
-                %s,
-                %s,
-                %s,
-                %s)
-        """, (
-            data['lat'],
-            data['lon'],
+        VALUES (%s, %s, %s, %s, %s, %s)
+        """,
+        (
+            data['city'],
             data['datetime'],
             data['temperature'],
             data['humidity'],
             data['wind_speed'],
-            data['raw_data']
-        ))
+            json.dumps(data['raw_data'])
+        )
+    )
 
     conn.commit()
     cur.close()
     conn.close()
+
+    print("Finished: Loading weather data")
