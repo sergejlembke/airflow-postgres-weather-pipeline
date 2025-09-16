f32b17663fa1f9c8e12cf6cb7d2972bf79bf36f9
sergejlembke <sergej.lembke@posteo.de>
2025-09-16 20:29:04 +0200

    Update load_weather_to_postgres function to use Airflow's XCom and adjust database insert parameters

--- scripts/load_weather.py
+++ scripts/load_weather.py
@@ -1,29 +1,40 @@
-import psycopg
-import json
-from datetime import datetime
+import psycopg2
 
-def load_weather_to_postgres(data, city="Berlin"):
-    conn = psycopg.connect(
+
+def load_weather_to_postgres(ti, city="Berlin"):
+    ti.log.info("Loading weather data")
+    data = ti.xcom_pull(task_ids='transform_weather')
+
+    # If running this script on the host machine, use localhost and port 5432.
+    # If running inside the Airflow container, use host="postgres" (the docker-compose service name).
+    conn = psycopg2.connect(
         dbname="weather",
         user="airflow",
         password="airflow",
-        host="postgres"
+        host="localhost",
+        port=5432,
     )
     cur = conn.cursor()
 
     cur.execute(
         """
-        # language=sql
         INSERT INTO weather_raw (city, datetime, temperature, humidity, wind_speed, raw_json)
-        VALUES (%s, %s, %s, %s, %s, %s)
+        VALUES (%s,
+                %s,
+                %s,
+                %s,
+                %s,
+                %s,
+                %s)
         """, (
-        city,
-        datetime.utcfromtimestamp(data["dt"]),
-        data["main"]["temp"],
-        data["main"]["humidity"],
-        data["wind"]["speed"],
-        json.dumps(data)
-    ))
+            data['lat'],
+            data['lon'],
+            data['datetime'],
+            data['temperature'],
+            data['humidity'],
+            data['wind_speed'],
+            data['raw_data']
+        ))
 
     conn.commit()
     cur.close()
