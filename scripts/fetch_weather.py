2d5236786de82073d8746bb23f4bc73ee19ae3af
sergejlembke <sergej.lembke@posteo.de>
2025-09-16 20:28:50 +0200

    Refactor get_weather function to retrieve API key internally

--- scripts/fetch_weather.py
+++ scripts/fetch_weather.py
@@ -1,13 +1,16 @@
+import json
 import os
 
 import requests
-import json
 
-def get_weather(lat, lon, api_key):
-    url = f'https://api.openweathermap.org/data/3.0/onecall?lat={lat}&lon={lon}&appid={api_key}'
+
+def get_weather(lat, lon):
+    api_key = get_api_key()
+    url = f'https://api.openweathermap.org/data/3.0/onecall?lat={lat}&lon={lon}&appid={api_key}&units=metric'
     response = requests.get(url)
     data = response.json()
     return data
+
 
 def get_api_key():
     config_path = os.path.join(os.path.dirname(__file__), '..', 'config.json')
@@ -15,11 +18,9 @@ def get_api_key():
         config = json.load(f)
     return config['api_key']
 
+
 lat = 52.52437
 lon = 13.41053
 
-api_key = get_api_key()
-
-weather = get_weather(lat, lon, api_key)
+weather = get_weather(lat, lon)
 print(json.dumps(weather, indent=4))
-
