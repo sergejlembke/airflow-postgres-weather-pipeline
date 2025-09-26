4b2068907b4b28e425790294fd85715a93ecd43a
sergejlembke <sergej.lembke@posteo.de>
2025-09-26 13:48:23 +0200

    Update to rename and refactor tables for consistent schema and improved clarity.

--- sql/create_tables.sql
+++ sql/create_tables.sql
@@ -1,20 +1,22 @@
-CREATE TABLE IF NOT EXISTS weather_raw (
-    id SERIAL PRIMARY KEY,
-    city TEXT,
-    datetime TIMESTAMP,
+CREATE TABLE IF NOT EXISTS weather_interval
+(
+    id          SERIAL PRIMARY KEY,
+    lat_lon     TEXT,
+    datetime    TIMESTAMP,
     temperature FLOAT,
-    humidity FLOAT,
-    wind_speed FLOAT,
-    raw_json JSONB,
-    created_at TIMESTAMP DEFAULT NOW()
+    humidity    FLOAT,
+    wind_speed  FLOAT,
+    raw_json    JSONB,
+    created_at  TIMESTAMP DEFAULT NOW()
 );
 
-CREATE TABLE IF NOT EXISTS weather_daily_summary (
-    id SERIAL PRIMARY KEY,
-    city TEXT,
-    date DATE,
-    avg_temp FLOAT,
-    max_temp FLOAT,
-    min_temp FLOAT,
+CREATE TABLE IF NOT EXISTS weather_daily_summary
+(
+    id         SERIAL PRIMARY KEY,
+    lat_lon    TEXT,
+    date       DATE,
+    avg_temp   FLOAT,
+    max_temp   FLOAT,
+    min_temp   FLOAT,
     created_at TIMESTAMP DEFAULT NOW()
 );
