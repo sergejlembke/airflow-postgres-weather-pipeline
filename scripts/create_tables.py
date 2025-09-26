f9e18550b34e2b9e56a7144fe503d4a8b75fa4e6
sergejlembke <sergej.lembke@posteo.de>
2025-09-26 13:47:47 +0200

    Update `create_tables.py` to use consistent single quotes for strings and improve code readability

--- scripts/create_tables.py
+++ scripts/create_tables.py
@@ -7,17 +7,17 @@ def create_tables():
     # If running this script on the host machine, use localhost and port 5432.
     # If running inside the Airflow Docker container, use host="postgres" (the docker-compose service name).
     conn = psycopg2.connect(
-        dbname="weather",
-        user="airflow",
-        password="airflow",
-        host="postgres"
+        dbname='weather',
+        user='airflow',
+        password='airflow',
+        host='postgres'
     )
     cur = conn.cursor()
 
     # create_tables.sql is located in the project-root/sql directory
     root_dir = os.path.dirname(os.path.dirname(__file__))
-    sql_path = os.path.join(root_dir, "sql", "create_tables.sql")
-    with open(sql_path, "r") as f:
+    sql_path = os.path.join(root_dir, 'sql', 'create_tables.sql')
+    with open(sql_path, 'r') as f:
         sql = f.read()
     cur.execute(sql)
 
