0b30ef3ec21c7a47845a71cd4a2b750ad5e873cc
sergejlembke <sergej.lembke@posteo.de>
2025-09-29 11:59:00 +0200

    Initialize response variable in extract_transform_load_raw.py

--- scripts/extract_transform_load_raw.py
+++ scripts/extract_transform_load_raw.py
@@ -56,6 +56,7 @@ def get_weather(ti) -> Dict[str, Any]:
         f'?lat={lat}&lon={lon}&exclude={exclude_param}&appid={api_key}&units={units}'
     )
 
+    response = None
     try:
         response = requests.get(url, timeout=30)
         # Raise for obvious HTTP errors
